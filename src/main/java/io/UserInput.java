c09b7ba87137a30062bf3d67d05cd82b658b2793
yewonniy <ae6652@naver.com>
2023-11-14 13:50:03 +0900

    fix: static으로 메서드 변경

--- src/main/java/io/UserInput.java
+++ src/main/java/io/UserInput.java
@@ -1,15 +1,15 @@
-package IO;
+package io;
 import camp.nextstep.edu.missionutils.*;
 
 public class UserInput {
-    public String readVisitDate() {
+    public static String readVisitDate() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n" +
                 "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
 
         return Console.readLine();
     }
 
-    public String readMenu() {
+    public static String readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
 
         return Console.readLine();
